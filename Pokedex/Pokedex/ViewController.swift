4bc020dc99ec57e158fcbae91acb0bfb52820f21
the-hye <ganzi410@gmail.com>
2024-07-08 11:05:58 +0900

    Setting SceneDelegate with NavigationController

--- Pokedex/Pokedex/ViewController.swift
+++ Pokedex/Pokedex/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.navigationItem.title = "Pokedex"
+        self.view.backgroundColor = .yellow
     }
 
 
