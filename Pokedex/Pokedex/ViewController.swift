cb52fa19f1abad45e66078754e3c4a51ad26bf46
the-hye <ganzi410@gmail.com>
2024-07-08 10:47:20 +0900

    Initialize Project, Remove Storyboard

---
+++ Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by JIHYE SEOK on 7/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
